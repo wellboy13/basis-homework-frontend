591fa511d4e0ff939caf35dd4ddf40a36094858d
wellboy13 <artzemchenko@gmail.com>
2023-10-23 15:31:31 +0200

    Upload homework 6

---
+++ homework6/script.js
@@ -0,0 +1,44 @@
+// 1. Напишите цикл for, который выводит консоль каждое второе число от 0 до 10
+
+for(let i = 0; i <= 10; i+=2) {
+    console.log(i)
+}
+
+
+// 2. Напишите цикл for, который выводит в консоль  все числа от 55 до 20    
+
+for(let i=55; i >= 20; i--) {
+    console.log(i)
+}
+
+
+// 3. Дан массив numbers. Вывести в консоль все числа из массива const numbers = [3, 5, 11, 2, 8, 1, 6]; Сформировать новый массив numbers_squared и передать в него все элементы из массива numbers, возведенные в квадрат
+
+const numbers = [3, 5, 11, 2, 8, 1, 6] // создаем массив
+console.log(numbers) // выводим массив в консоль
+
+const numbers_squared = [] // создаем пустой массив
+
+for(let i = 0; i < numbers.length; i++) { 
+    numbers_squared.push(numbers[i] ** 2)
+} // запускаем цикл для заполнения новго массива числами в **2
+
+
+// 4. Создать переменную last_elem и передать в нее последний элемент из сформированного массива numbers_squared (обратиться к элементу массива по индексу)
+
+let last_elem = numbers_squared[6] //получаем значение последней ячейки массива numbers_squared
+// или...
+//let last_elem = numbers_squared.pop() //удалит и вернет последний элемент массива переменной let last_elem
+
+
+
+// 5. Дан объект user. Используя данные из объекта, сформировать строку в формате: ‘User’s name is <FIRST_NAME> <LAST_NAME>. He is <AGE> years old’
+
+const user = { // создаем объект с данными
+    first_name: "Ivan",
+    last_name: "Ivanov",
+    age: 20,
+    salary: 500
+}
+console.log(`Users\`s name is ${user.first_name} ${user.last_name}. He is ${user.age} years old`) // выводим в консоль строку с подстановкой данных объекта
+
